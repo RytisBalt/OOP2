f1374facf55d7c93deb800422d5995d32f20d5d5
Rytis B <31890930+RytisBalt@users.noreply.github.com>
2022-10-17 14:52:57 +0300

    Update funkcijos.cpp

--- funkcijos.cpp
+++ funkcijos.cpp
@@ -12,64 +12,59 @@ unsigned int zodziu_kiekis(string const& str) {
     return distance(istream_iterator<string>(stream), istream_iterator<string>());
 }
 
-void skaitymas(vector<studentas>& kursas, int* pkiekis) {
+void skaitymas(vector<studentas>& kursas) {
     int skiekis = 0;
     int temp;
     ifstream file;
     string buff;
+    studentas s;
     file.open("kursiokai.txt");
     try {
         if (!file.is_open())
         {
             throw 333;
         }
+        cout << setw(25) << left << "Studento vardas" << setw(25) << left << "Pavarde" << setw(20) << left << "Galutinis(vidurkis)/ Galutinis(mediana)"
+            << "--------------------------\n";
         getline(file >> ws, buff);
-        *pkiekis = zodziu_kiekis(buff) - 3;
+        int pkiekis = zodziu_kiekis(buff) - 3;
         while (true) {
-            kursas.resize(kursas.size() + 1);
-            file >> kursas.at(skiekis).vardas;
+            //kursas.resize(kursas.size() + 1);
             if (file.eof()) {
                 kursas.pop_back();
                 break;
             }
-            file >> kursas.at(skiekis).pavarde;
-            for (int i = 0; i < *pkiekis; ++i) {
+            file >> s.vardas;
+            file >> s.pavarde;
+            for (int i = 0; i < 15; ++i) {
                 file >> temp;
-                kursas.at(skiekis).nd.push_back(temp);
+                s.nd.push_back(temp);
             }
-            file >> kursas.at(skiekis).egz;
-            kursas.at(skiekis).galutinis = kursas.at(skiekis).galutinis * 0.4 + 0.6 * kursas.at(skiekis).egz;
-            skiekis++;
+            file >> s.egz;
+            kursas.push_back(s);
+            s.nd.clear();
         }
     }
     catch (int x) {
         cout << "NEATIDARYTAS FAILAS, NEIMANOMA UZPILDYTI LENTELES. ERRORO NUMERIS: " << x << endl << endl;
     }
 }
-void spausdinimas(vector<studentas> kursas, int pkiekis) {
+void spausdinimas(vector<studentas>& kursas) {
 
-    cout << setw(25) << left << "Studento vardas" << setw(25) << left << "Pavarde" << setw(20) << left << "Galutinis(vidurkis)/ Galutinis(mediana)"
-        << "--------------------------\n";
     for (int i = 0; i < kursas.size(); i++) {
-        cout << setw(25) << left << kursas[i].vardas << setw(25) << left << kursas[i].pavarde << setw(20) << left << kursas[i].galutinis << mediana(kursas[i].nd) << endl;
+        kursas[i].galutinisv = (vidurkis(kursas[i].nd) * 0.4) + (kursas[i].egz * 0.6);
+        kursas[i].galutinism = (mediana(kursas[i].nd) * 0.4) + (kursas[i].egz * 0.6);
+        cout << setw(25) << left << kursas[i].vardas << setw(25) << left << kursas[i].pavarde << setw(20) << left << kursas[i].galutinisv << kursas[i].galutinism << endl;
     }
 }
 float mediana(vector<int> nd) {
-    int counter = 0;
-    for (int i = 0; i < 10; i++) {
-        if (nd[i] != 0) {
-            counter++;
-        }
-        if (nd[i] == -1) {
-            nd[i] = 0;
-            counter--;
-            break;
-        }
-    }
-    if (counter % 2 == 0) {
-        return float(((nd[counter / 2 - 1]) + (nd[(counter / 2)])) / 2.0);
-    }
-    else {
-        return float(nd[(counter / 2)]);
-    }
+    typedef vector<int>::size_type ndSize;
+    ndSize size = nd.size();
+    sort(nd.begin(), nd.end());
+    ndSize vid = size / 2;
+    return size % 2 == 0 ? (nd[vid] + nd[vid - 1]) / 2 : nd[vid];
+}
+float vidurkis(vector<int> nd) {
+    return accumulate(nd.begin(), nd.end(), 0.0) / nd.size();
+
 }
