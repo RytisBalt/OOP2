fcaadfbb3ba0a9cbd392d60d3331f1638a6f53fd
Rytis B <31890930+RytisBalt@users.noreply.github.com>
2022-10-22 11:29:29 +0300

    Update 3 projektas.cpp

--- 3 projektas.cpp
+++ 3 projektas.cpp
@@ -14,15 +14,15 @@ int main() {
 
 
     vector<studentas> kursas;
-    analize(1000, 5, kursas);
+    //analize(1000, 5, kursas);
     cout << endl;
     analize(10000, 5, kursas);
     cout << endl;
-    analize(100000, 5, kursas);
+    //analize(100000, 5, kursas);
     cout << endl;
-    analize(1000000, 5, kursas);
+    //analize(1000000, 5, kursas);
     cout << endl;
-    analize(10000000, 5, kursas);
+    //analize(10000000, 5, kursas);
     cout << endl;
     system("pause");
     return 0;
